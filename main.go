a0bea5e1392a6abc5c97d1072031f42da88f8bc4
keelus <hugomoreda@hotmail.com>
2023-07-09 15:58:44 +0200

    Added settings and save functionality

--- main.go
+++ main.go
@@ -4,13 +4,39 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"strings"
+	"text/template"
+	"time"
 
+	"github.com/forestgiant/sliceutil"
 	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
+
+//
+//	TODO:
+//	# LED MANAGEMENT
+//		Led management[save on a JSON and load in memory]
+//		Struct for types of LED [static, rainbow, gradient]
+//		For -static: just hex
+//		For -rainbow: if all-> just save mode. if specific-> save color spectrum
+//		For -gradient: if all-> just save mode. if specific-> save color spectrum
+//
+//		{
+//				"mode":"X",
+//			"spectrum": ["","",...] // if all or static: "spectrum":[]
+//		}
+//
+//		Option to save presets
+
+//	# DESIGN & PERSONALISATION
+//		Load and save system. Compatibility with scss/css
+//
 
 type Configuration struct {
 	Version         string
@@ -58,25 +84,31 @@ var userConfiguration Configuration
 func renderer() multitemplate.Renderer {
 	r := multitemplate.NewRenderer()
 
-	r.AddFromFiles("login", "templates/login.html")
-	r.AddFromFiles("panel", "templates/panel.html")
+	funcs := template.FuncMap{
+		"fPreventCaching": func() string {
+			rand.Seed(time.Now().UnixNano())
+			characters := "0123456789abcdefghijklmnopqrstuvwxyz"
+
+			randomBytes := make([]byte, 5)
+			for i := range randomBytes {
+				randomBytes[i] = characters[rand.Intn(len(characters))]
+			}
+
+			return fmt.Sprintf("?preventCaching=%s", string(randomBytes))
+		},
+	}
+
+	r.AddFromFilesFuncs("login", funcs, "templates/login.html")
+	r.AddFromFilesFuncs("panel", funcs, "templates/panel.html")
 
 	return r
 }
 
 func init() {
-	// Read the JSON file
-	data, err := ioutil.ReadFile("userConfig.json")
+	var err error
+	userConfiguration, err = loadUserConfiguration()
 	if err != nil {
-		fmt.Println("Error reading JSON file:", err)
-		return
-	}
-
-	// Unmarshal JSON data into the Configuration struct
-	err = json.Unmarshal(data, &userConfiguration)
-	if err != nil {
-		fmt.Println("Error unmarshaling JSON:", err)
-		return
+		fmt.Println("User configuration could not be loaded...")
 	}
 }
 func main() {
@@ -98,9 +130,11 @@ func main() {
 
 	store := cookie.NewStore([]byte("secret"))
 	store.Options(sessions.Options{
-		Path:     "/",
-		Secure:   true,
-		HttpOnly: true,
+		Path: "/",
+		// Secure:   true,
+		Secure: false,
+		// HttpOnly: true,
+		HttpOnly: false,
 		SameSite: http.SameSiteStrictMode,
 		MaxAge:   cookieAge,
 	})
@@ -146,6 +180,7 @@ func main() {
 			"UserConfiguration": userConfiguration,
 			"LatestVersion":     latestVersion,
 			"UsingLatest":       usingLatest,
+			"CurrentLED":        "#00FF00",
 		})
 	})
 
@@ -191,28 +226,142 @@ func main() {
 	r.POST("/back/saveConfiguration/:category", func(c *gin.Context) {
 		category := c.Param("category")
 
+		returnError := []map[string]string{}
+
 		switch category {
-		case "userInfo":
+		case "UserInfo":
+			username := c.PostForm("Username")
+			password := c.PostForm("Password")
+
+			if len(username) < 3 || len(username) > 20 {
+				returnError = append(returnError, map[string]string{"details": "The username must be at least 3 characters long, with a maximum of 20.", "field": "userInfo0"})
+			}
+
+			if strings.TrimSpace(password) != "" {
+				if len(password) < 4 {
+					returnError = append(returnError, map[string]string{"details": "The password must be at least 4 characters long.", "field": "userInfo1"})
+				}
+			}
+
+			if len(returnError) == 0 {
+				userConfiguration.UserInfo.Username = username
+				if strings.TrimSpace(password) != "" {
+					userConfiguration.UserInfo.Password = password
+				}
+			}
 			break
-		case "sessionSettings":
+		case "SessionSettings":
+			lifespanString := c.PostForm("Lifespan")
+			unit := c.PostForm("Unit")
+
+			allowedUnits := []string{"min", "hour", "day", ""}
+
+			fmt.Printf("lifespan:'%s'\n", lifespanString)
+			fmt.Printf("unit:'%s'\n", unit)
+			if !sliceutil.Contains(allowedUnits, unit) { // Replace to array instead of slice TODO
+				returnError = append(returnError, map[string]string{"details": "Unexpected time unit.", "field": "sessionDuration3"})
+			}
+
+			lifespan, err := strconv.Atoi(lifespanString)
+			if err != nil {
+				returnError = append(returnError, map[string]string{"details": "Unexpected lifespan format.", "field": "sessionDuration2"})
+			}
+
+			if len(returnError) == 0 {
+				if unit == "" {
+					userConfiguration.SessionSettings.Unit = ""
+					userConfiguration.SessionSettings.Lifespan = -1
+				} else {
+					userConfiguration.SessionSettings.Unit = unit
+					userConfiguration.SessionSettings.Lifespan = lifespan
+				}
+			}
 			break
-		case "ledControl":
+		case "LedControl":
+			ledControlString := c.PostForm("LedControl")
+
+			var ledControl [2]bool
+			err := json.Unmarshal([]byte(ledControlString), &ledControl)
+			if err != nil {
+				returnError = append(returnError, map[string]string{"details": "Unexpected format.", "field": ""})
+			}
+
+			if len(returnError) == 0 {
+				userConfiguration.LedControl = ledControl
+			}
 			break
-		case "systemStats":
+		case "SystemStats":
+			systemStatsString := c.PostForm("SystemStats")
+
+			var systemStats [5]bool
+			err := json.Unmarshal([]byte(systemStatsString), &systemStats)
+			if err != nil {
+				returnError = append(returnError, map[string]string{"details": "Unexpected format.", "field": ""})
+			}
+
+			if len(returnError) == 0 {
+				userConfiguration.SystemStats = systemStats
+			}
 			break
-		case "logging":
+		case "Logging":
+			loggingString := c.PostForm("Logging")
+
+			logging, err := strconv.ParseBool(loggingString)
+			if err != nil {
+				returnError = append(returnError, map[string]string{"details": "Unexpected format.", "field": ""})
+			}
+
+			if len(returnError) == 0 {
+				userConfiguration.Logging = logging
+			}
 			break
-		case "security":
+		case "Security":
+			userInputString := c.PostForm("UserInput")
+
+			userInput, err := strconv.ParseBool(userInputString)
+			if err != nil {
+				returnError = append(returnError, map[string]string{"details": "Unexpected format.", "field": ""})
+			}
+
+			if len(returnError) == 0 {
+				userConfiguration.Security.UserInput = userInput
+			}
 			break
-		case "region":
+		case "Units":
+			timeMode := c.PostForm("TimeMode")
+			temperature := c.PostForm("Temperature")
+
+			allowedTimeMode := []string{"12", "24"}
+			allowedTemperature := []string{"C", "F"}
+
+			if !sliceutil.Contains(allowedTimeMode, timeMode) { // Replace to array instead of slice TODO
+				returnError = append(returnError, map[string]string{"details": "Unexpected time unit.", "field": "units0"})
+			}
+			if !sliceutil.Contains(allowedTemperature, temperature) { // Replace to array instead of slice TODO
+				returnError = append(returnError, map[string]string{"details": "Unexpected temperature unit.", "field": "units1"})
+			}
+
+			if len(returnError) == 0 {
+				userConfiguration.Units.TimeMode12 = timeMode == "12"
+				userConfiguration.Units.TemperatureC = temperature == "C"
+			}
 			break
-		case "design":
-			break
-		case "updates":
-			break
-		case "deletion":
+		case "Design":
+			// TODO
 			break
 		}
+
+		if len(returnError) == 0 {
+			err := saveUserConfiguration()
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, map[string]string{"details": "Error while saving the user configuration. 500", "field": ""})
+			} else {
+				c.Status(http.StatusOK)
+			}
+			return
+		}
+
+		c.JSON(http.StatusBadRequest, returnError)
 	})
 
 	r.Run(":80")
@@ -229,4 +378,38 @@ func isLogged(c *gin.Context) bool {
 		return true
 	}
 	return false
+}
+
+func loadUserConfiguration() (Configuration, error) {
+	var config Configuration
+
+	data, err := ioutil.ReadFile("userConfig.json")
+	if err != nil {
+		fmt.Println("Error reading JSON file:", err)
+		return config, err
+	}
+
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		fmt.Println("Error unmarshaling JSON:", err)
+		return config, err
+	}
+
+	return config, nil
+}
+
+func saveUserConfiguration() error {
+	data, err := json.MarshalIndent(userConfiguration, "", "	")
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return err
+	}
+
+	err = ioutil.WriteFile("userConfig.json", data, 0644)
+	if err != nil {
+		fmt.Println("Error writing JSON file:", err)
+		return err
+	}
+
+	return nil
 }
