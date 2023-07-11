a1fbd495033ac64421b7df72fe56f498228c40e2
keelus <hugomoreda@hotmail.com>
2023-07-12 01:04:31 +0200

    :sparkles: First steps into installation

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"horus/config"
 	"horus/server"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 //	TODO:
@@ -13,7 +17,51 @@ import (
 
 const CUR_VERSION = "0.7.0"
 
+func init() {
+	fmt.Println("Initializing horus...")
+	if !fileExists("config/userConfig.yaml") {
+		fmt.Println("User configuration has not been found. Initializing horus setup...")
+		config.Init()
+		setupConfig()
+		config.SaveAll()
+		// os.Exit(-1)
+	}
+
+}
 func main() {
-	config.Init()
+	// config.Init()
 	server.Init()
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func setupConfig() {
+	var username string
+	var password string
+
+	fmt.Println("HORUS setup:")
+
+	fmt.Print("\t-Username:")
+	fmt.Scan(&username)
+	fmt.Print("\t-Password:\033[8m")
+
+	fmt.Scan(&password)
+	fmt.Print("\033[28m")
+
+	cryptedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println("Unexpected error happened while encrypting your password.")
+		os.Exit(-1)
+	}
+
+	config.UserConfiguration.UserInfo.Username = username
+	config.UserConfiguration.UserInfo.Password = string(cryptedPassword)
+
+	fmt.Println("Configuration done. Initializing horus...")
 }
