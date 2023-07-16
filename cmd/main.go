6d1de9dd342073e2cf696fe34cd95738510db0ae
keelus <hugomoreda@hotmail.com>
2023-07-16 20:25:08 +0200

    :bookmark: Version 0.7.5 - Breathing color

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ import (
 //	# DESIGN & PERSONALISATION
 //		Load and save system. Compatibility with scss/css
 
-const CUR_VERSION = "0.7.4"
+const CUR_VERSION = "0.7.5"
 
 func init() {
 	fmt.Println("Initializing horus...")
