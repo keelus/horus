fb77424be27fbe52e698eab6e62cdcabe0fcc97c
keelus <hugomoreda@hotmail.com>
2023-07-17 20:07:09 +0200

    :bookmark: Added "beta" to version

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ import (
 //	# DESIGN & PERSONALISATION
 //		Load and save system. Compatibility with scss/css
 
-const CUR_VERSION = "0.7.8"
+const CUR_VERSION = "0.7.8 beta"
 
 func init() {
 	fmt.Println("Initializing horus...")
