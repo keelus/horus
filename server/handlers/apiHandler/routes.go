f359af7e5ec390347a298f465daf8eb5e0c64971
keelus <hugomoreda@hotmail.com>
2023-07-17 18:11:53 +0200

    :construction: First steps into system stats

--- server/handlers/apiHandler/routes.go
+++ server/handlers/apiHandler/routes.go
@@ -3,7 +3,14 @@ package apiHandler
 import (
 	"horus/config"
 	"horus/internal"
+	"io/ioutil"
+	"os/exec"
+	"strconv"
+	"strings"
+
 	"net/http"
+
+	"horus/main"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -49,14 +56,67 @@ func HandleLogout(c *gin.Context) {
 	c.Redirect(http.StatusTemporaryRedirect, "/")
 }
 func HandleGetStats(c *gin.Context) {
-	// c.Status(http.StatusBadGateway)
+	var temperature float64
+	var cpuUsage int
+	var ramUsage int
+	var diskUsage [2]int
+	var sysUptime float64
+
+	if main.CUR_RASP {
+		temperature, _ = getTemp()
+		cpuUsage, _ = getCpuUsage()
+		ramUsage, _ = getRamUsage()
+		diskUsage, _ = getDiskUsage()
+		sysUptime, _ = getSysUptime()
+	} else {
+		temperature = float64(internal.RandomValue(0, 85))
+		cpuUsage = internal.RandomValue(0, 100)
+		ramUsage = internal.RandomValue(0, 100)
+		diskUsage = [2]int{internal.RandomValue(0, 120000), 120000}
+		sysUptime = float64(internal.RandomValue(0, 100000))
+	}
 
 	c.JSON(http.StatusOK, gin.H{ // TODO : Change to receive real data. Placeholder for now.
-		"Temperature": internal.RandomValue(0, 85),
-		"CPU":         internal.RandomValue(0, 100),
-		"RAM":         internal.RandomValue(0, 100),
-		"Disk":        internal.RandomValue(0, 120000), // MB
-		"DiskMax":     120000,                          // MB
-		"Uptime":      internal.RandomValue(0, 100000),
+		"Temperature": temperature,
+		"CPU":         cpuUsage,
+		"RAM":         ramUsage,
+		"Disk":        diskUsage[0],
+		"DiskMax":     diskUsage[1],
+		"Uptime":      sysUptime,
 	})
+}
+
+func getTemp() (float64, error) {
+	cmd := exec.Command("vcgencmd", "measure_temp")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return 0, err
+	}
+
+	value := strings.TrimPrefix(string(output), "temp=")
+	value = strings.Replace(value, "'C", "", 1)
+
+	valueF64, _ := strconv.ParseFloat(value, 64)
+	return valueF64, nil
+}
+func getCpuUsage() (int, error) {
+	return 0, nil
+}
+func getRamUsage() (int, error) {
+	return 0, nil
+}
+func getDiskUsage() ([2]int, error) {
+	return [2]int{0, 0}, nil
+}
+func getSysUptime() (float64, error) {
+	data, err := ioutil.ReadFile("/proc/uptime")
+	if err != nil {
+		return 0, err
+	}
+
+	uptime := strings.Fields(string(data))[0]
+
+	uptimeF64, _ := strconv.ParseFloat(uptime, 64)
+
+	return uptimeF64, nil
 }
