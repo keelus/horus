2abdfb9f9755db4beded83a141f2c92c60a14884
keelus <hugomoreda@hotmail.com>
2023-07-17 16:57:38 +0200

    :art: Structs compressed

--- models/structs.go
+++ models/structs.go
@@ -2,56 +2,43 @@ package models
 
 // USER CONFIGURATION
 type Configuration struct {
-	Version         string          `yaml:"Version"`
-	UserInfo        UserInfo        `yaml:"UserInfo"`
-	SessionSettings SessionSettings `yaml:"SessionSettings"`
-	LedControl      [3]bool         `yaml:"LedControl"`
-	SystemStats     [5]bool         `yaml:"SystemStats"`
-	Logging         bool            `yaml:"Logging"`
-	Security        Security        `yaml:"Security"`
-	Units           Units           `yaml:"Units"`
-	Design          Design          `yaml:"Design"`
-}
-
-type UserInfo struct {
-	Username string `yaml:"Username"`
-	Password string `yaml:"Password"`
-}
-
-type SessionSettings struct {
-	Lifespan int    `yaml:"Lifespan"`
-	Unit     string `yaml:"Unit"`
-}
-
-type Security struct {
-	UserInput bool `yaml:"UserInput"`
-}
-
-type Units struct {
-	TimeMode12   bool `yaml:"TimeMode12"`
-	TemperatureC bool `yaml:"TemperatureC"`
-}
-
-type Design struct {
-	Accent []string      `yaml:"Accent"`
-	Fonts  []FontDetails `yaml:"Fonts"`
-}
-
-type FontDetails struct {
-	Title  string `yaml:"Title"`
-	Source string `yaml:"Source"`
+	Version  string `yaml:"Version"`
+	UserInfo struct {
+		Username string `yaml:"Username"`
+		Password string `yaml:"Password"`
+	} `yaml:"UserInfo"`
+	SessionSettings struct {
+		Lifespan int    `yaml:"Lifespan"`
+		Unit     string `yaml:"Unit"`
+	} `yaml:"SessionSettings"`
+	LedControl  [3]bool `yaml:"LedControl"`
+	SystemStats [5]bool `yaml:"SystemStats"`
+	Logging     bool    `yaml:"Logging"`
+	Security    struct {
+		UserInput bool `yaml:"UserInput"`
+	} `yaml:"Security"`
+	Units struct {
+		TimeMode12   bool `yaml:"TimeMode12"`
+		TemperatureC bool `yaml:"TemperatureC"`
+	} `yaml:"Units"`
+	Design struct {
+		Accent []string `yaml:"Accent"`
+		Fonts  []struct {
+			Title  string `yaml:"Title"`
+			Source string `yaml:"Source"`
+		} `yaml:"Fonts"`
+	} `yaml:"Design"`
 }
 
 // LED
 type LedPresets struct {
-	StaticColor    []string       `yaml:"StaticColor"`
-	StaticGradient [][]string     `yaml:"StaticGradient"`
-	FadingRainbow  int            `yaml:"FadingRainbow"`
-	BreathingColor BreathingColor `yaml:"BreathingColor"`
-}
-type BreathingColor struct {
-	Cooldown int      `yaml:"Cooldown"`
-	Colors   []string `yaml:"Colors"`
+	StaticColor    []string   `yaml:"StaticColor"`
+	StaticGradient [][]string `yaml:"StaticGradient"`
+	FadingRainbow  int        `yaml:"FadingRainbow"`
+	BreathingColor struct {
+		Cooldown int      `yaml:"Cooldown"`
+		Colors   []string `yaml:"Colors"`
+	} `yaml:"BreathingColor"`
 }
 type LedActive struct {
 	ActiveMode string   `yaml:"ActiveMode"`
