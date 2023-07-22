3267de2027b05a3b8e5c5e510bc8330e23d1fa53
keelus <hugomoreda@hotmail.com>
2023-07-22 15:48:13 +0200

    :bookmark: 0.9.3 beta - Gradient editing & color pickers

--- internal/utils.go
+++ internal/utils.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-const CUR_VERSION = "0.9.2 beta"
+const CUR_VERSION = "0.9.3 beta"
 
 func IsLogged(c *gin.Context) bool {
 	session := sessions.Default(c)
